8f22b9d98b0de86bb3c553084d16050deb1f52ca
Lee Jeongmin <102647148+min1018@users.noreply.github.com>
2024-07-02 15:47:58 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,7 +1,6 @@
--- 코드를 작성해주세요
 SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
-FROM DEVELOPERS 
+FROM DEVELOPERS
 WHERE SKILL_CODE & (SELECT SUM(CODE) 
                    FROM SKILLCODES
-                   WHERE CATEGORY LIKE 'Front%') > 0
+                   WHERE CATEGORY LIKE 'Front%')
 ORDER BY ID;
