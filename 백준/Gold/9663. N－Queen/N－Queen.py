8bb60c0385c51dcb36b811062c5d4a037844851b
Lee Jeongmin <102647148+min1018@users.noreply.github.com>
2024-07-11 12:39:54 +0900

    [Gold IV] Title: N-Queen, Time: 28244 ms, Memory: 198984 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,25 +1,25 @@
-import sys
-input = sys.stdin.readline
+# 열 같으면 안됨, 행 같으면 안됨 
+# 대각선이면 안됨 현재 퀸으로부터 x좌표 = y좌표 
 
-def adjacent(x):
-    for i in range(x):
-        if row[x] == row[i] or abs(row[x] - row[i]) == x -i:
-            return False
-    return True
+def promising(x):
+  for i in range(x):
+    if row[x] == row[i] or abs(row[x] - row[i]) == abs(x-i):
+      return False
+  return True
+    
 
-def dfs(x):
-    global result 
-    if x == n:
-        result += 1
-    else:
-        for i in range(n):
-            row[x] = i
-            if adjacent(x):
-                dfs(x+1)
+def dfs(cnt):
+  global ans 
+  if cnt == n:
+    ans += 1
+    return 
+  for i in range(n):
+    row[cnt] = i
+    if promising(cnt): #백
+      dfs(cnt + 1)
 
-n = int(input().strip())
+n = int(input())
 row = [0] * n
-result = 0
-# print(row)
+ans = 0
 dfs(0)
-print(result)
+print(ans)
