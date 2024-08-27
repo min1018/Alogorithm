2e6c65afd37f523ef314b91116d0e440eb5fff43
Lee Jeongmin <102647148+min1018@users.noreply.github.com>
2024-08-27 18:46:49 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 277.93 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,13 +1,11 @@
-from collections import deque
 def solution(elements):
-    answer = set()
-    q = deque(elements)
-    for i in range(len(q)):
-        sum = 0
-        for e in q:
-            sum += e
-            if not sum in answer: ## 조건문추가
-            	answer.add(sum)   
-        q.append(q.popleft())
+    length = len(elements)
+    poss = set()
 
-    return len(answer)
+    for i in range(length):
+        ssum = elements[i]
+        poss.add(ssum)
+        for k in range(i+1, i+length):
+            ssum += elements[k%length]
+            poss.add(ssum)
+    return len(poss)
