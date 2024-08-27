0dad0b525e716b0e3ade453e3f6e59d247163d0b
Lee Jeongmin <102647148+min1018@users.noreply.github.com>
2024-08-27 18:09:05 +0900

    [level 2] Title: 택배상자, Time: 271.28 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -0,0 +1,15 @@
+def solution(order):
+    answer = 0
+    
+    belt = []
+    idx = 0
+    for box in range(1, len(order) + 1):
+        belt.append(box)
+        while belt and belt[-1] == order[idx]:
+            answer += 1 
+            idx += 1
+            belt.pop()
+    
+    return answer 
+
+# 맨앞의 상자만 뺄 수 있음 -> 스택 
