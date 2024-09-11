9c2382edc33cc23569c43299699d0019f69183a2
Lee Jeongmin <102647148+min1018@users.noreply.github.com>
2024-09-11 23:04:09 +0900

    [level 2] Title: 과제 진행하기, Time: 1.57 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,9 +1,43 @@
+
+
 def solution(plans):
+    # 시간 변환 
+    for i in range(len(plans)):
+        subject, time, take = plans[i]
+        start = int(time.split(":")[0])*60 + int(time.split(":")[1])
+        plans[i] = [subject, start, int(take)]
+    plans.sort(key = lambda x : (x[1]))   
+    
+    stop = []
     answer = []
     for i in range(len(plans)):
-        work = plans[i]
-        cur_time = int(work[1].split(":")[0]) * 60 + int(work[1].split(":")[1])
-        plans[i] = [work[0], cur_time, int(work[2])]
-    plans.sort(key = lambda x: x[1])    
-    print(plans)
+        if i == len(plans)-1:
+            answer.append(plans[i][0])
+            break
+            
+        currsubject, currtime, currdur = plans[i]
+        nxtsubject, nxttime, nxtdur = plans[i+1]
+        
+        if currtime + currdur <= nxttime:
+            answer.append(currsubject) # 완료 
+            
+            lefttime = nxttime - (currtime + currdur)
+            while lefttime != 0 and stop:
+                tmpsubject, tmptime, tmpdur = stop.pop()
+                if tmpdur <= lefttime: # 수행 끝내기 가능 
+                    lefttime -= tmpdur
+                    answer.append(tmpsubject)
+                else: # 불가 다시 돌아가
+                    tmpdur -= lefttime
+                    lefttime = 0
+                    stop.append([tmpsubject, tmptime, tmpdur])
+        
+        else:
+            stop.append([currsubject, currtime, currdur - (nxttime-currtime)])
+        
+    while stop:
+        sub, _, _ = stop.pop()
+        answer.append(sub)
+        
     return answer
+# 새로운 과제 > 멈춘 과제, 진행중인 과제 
