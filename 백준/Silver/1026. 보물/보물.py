cf3f03d706283418d82e62ec0e908ddf19a5fea0
Lee Jeongmin <102647148+min1018@users.noreply.github.com>
2024-09-30 18:53:13 +0900

    [Silver IV] Title: 보물, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,10 @@
+n = int(input())
+A = list(map(int, input().split(" ")))
+B = list(map(int, input().split(" ")))
+A.sort()
+B.sort(reverse = True)
+
+S = 0
+for a, b in zip(A, B):
+    S += a*b
+print(S)
